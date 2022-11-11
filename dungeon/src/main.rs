e17776518568afff4f710620419fc847f77919e9
lykimq <lykimq@gmail.com>
2022-11-11 09:03:02 +0700

    fix comment

--- dungeon/src/main.rs
+++ dungeon/src/main.rs
@@ -111,7 +111,7 @@ fn main() -> BError {
       why we need a camera.
     */
 
-    /* run with cargo run --release */
+    /* run with cargo run --release; or with cargo run */
     let context = BTermBuilder::new()
         .with_title("Dungeon Crawler")
         .with_fps_cap(30.0)
@@ -119,7 +119,6 @@ fn main() -> BError {
         //.with_dimensions(DISPLAY_WIDTH, DISPLAY_HEIGHT)
         //.with_tile_dimensions(32, 32)
         .with_resource_path("resources/")
-        // Todo where is this file??
         .with_font("dungeonfont.png", 16, 16)
         .with_simple_console(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
         .with_simple_console_no_bg(DISPLAY_WIDTH, DISPLAY_HEIGHT, "dungeonfont.png")
